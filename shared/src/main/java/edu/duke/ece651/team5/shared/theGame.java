65f5f3e2049e3e6aa25cc8f5a7dec879ca8250b7
Santiago Brito <sjb103@duke.edu>
2023-03-15 19:17:49 -0400

    added initPlayers, need testing for this

--- shared/src/main/java/edu/duke/ece651/team5/shared/theGame.java
+++ shared/src/main/java/edu/duke/ece651/team5/shared/theGame.java
@@ -1,15 +1,19 @@
 package edu.duke.ece651.team5.shared;
 
 import java.io.BufferedReader;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class theGame {
     
     private RISKMap theMap;
+    private final BufferedReader inputReader;
 
     public theGame(){
 
+        this.inputReader = new BufferedReader(new InputStreamReader(System.in));
         this.theMap = new RISKMap();
 
     }
@@ -17,7 +21,7 @@ public class theGame {
 
     public int howManyPlayers() throws IOException{
 
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        
 
         Integer numberOfPlayers = null;
         String problem = null;
@@ -25,7 +29,7 @@ public class theGame {
             problem = null;
             try {
                 System.out.println("How many players would you like to have in this game?");
-                numberOfPlayers = Integer.parseInt(input.readLine());
+                numberOfPlayers = Integer.parseInt(inputReader.readLine());
 
                 if(numberOfPlayers <= 0){
                     throw new IllegalArgumentException("The number of players can't  be negative or zero");
@@ -51,4 +55,35 @@ public class theGame {
         return numberOfPlayers;
     }
 
+    public ArrayList<Player> initPlayers() throws IOException{
+
+        Integer numberOfPlayers = howManyPlayers();
+        ArrayList<Player> thePlayers = new ArrayList<>();
+        
+        for (int playerX = 0; playerX < numberOfPlayers; playerX++){
+
+            String name = null;
+            String problem = null;
+            do {
+                problem = null;
+                try{
+
+                    System.out.println("What name would you like?");
+                    name = inputReader.readLine();
+                }
+
+                //TODO here we need to add error checking for players that have same name
+                catch (EOFException iae){
+                    problem = "the name cannot be empty";
+
+                }
+
+            } while (problem != null);
+
+            Player X = new Player(name);
+            thePlayers.add(X);
+        }
+
+
+    }
 }

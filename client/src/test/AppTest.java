9042dbcf2e54a1e8ac6f0a1c42a5540830890631
mz213 <mz213@duke.edu>
2023-03-19 14:20:48 -0400

    finish move and skeleton attack

---
+++ client/src/test/AppTest.java
@@ -0,0 +1,17 @@
+package edu.duke.ece651.team5.client;
+
+import org.junit.jupiter.api.Test;
+
+import edu.duke.ece651.team5.client.App;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
+
+class AppTest {
+  @Test
+  void test_GetMessage() {
+    App a = new App();
+    assertEquals("Hello from the client for team5", a.getMessage());
+  }
+}

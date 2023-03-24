04c69e110f6771cc318388c261581b0b789e0e82
mz213 <mz213@duke.edu>
2023-03-24 19:55:48 -0400

    Replace ClientTest.java

--- client/src/test/java/edu/duke/ece651/team5/client/ClientTest.java
+++ client/src/test/java/edu/duke/ece651/team5/client/ClientTest.java
@@ -33,20 +33,7 @@ public class ClientTest {
     client.createPlayer();
     return client;
   }
-
-  @Test
-  void testCreatePlayer() throws IOException, ClassNotFoundException{
-    PlayerConnection test = mock(PlayerConnection.class);
-    when(test.readData()).thenReturn("test");
-    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-    BufferedReader input = new BufferedReader(new StringReader("1"));
-    PrintStream output = new PrintStream(bytes, true);
-    Client client = createPlayer(test, input, output);
-    client.createPlayer();
-    String data = (String)client.playerConnection.readData();
-    assertEquals("test", data);
-  }
-
+  
 
 
 
@@ -264,12 +251,12 @@ public class ClientTest {
     PlayerConnection test = mock(PlayerConnection.class);
     when(test.readData()).thenReturn(attRes, res);
     ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-    BufferedReader input = new BufferedReader(new StringReader("Disconnect"));
+    BufferedReader input = new BufferedReader(new StringReader("1"));
     PrintStream output = new PrintStream(bytes, true);
 
     Client client = createPlayer(test, input, output);
     client.textPlayer.setPlayerName("Green");
-    String msg = client.checkResult();
+    client.checkResult();
     String expected = 
     "This is your player name for this game: Green\n" +
     "Your attack order to attack Territory B was lose in last round.\n\n" + 
@@ -280,14 +267,13 @@ public class ClientTest {
     "2. Quit the game\n" + 
     "Please enter 1 or 2\n\n";
     assertEquals(expected, bytes.toString());
-    assertEquals("Disconnect", msg);
 
   }
 
   @Test
   void testCheckResult2() throws IOException, ClassNotFoundException{
     HashMap<String, Boolean> res = new HashMap<>();
-    res.put("Green", null);
+    res.put("Green", true);
     res.put("Red", null);
     ArrayList<AttackOrder> attRes = createAttRes(1);
 		PlayerConnection test = mock(PlayerConnection.class);
@@ -298,10 +284,10 @@ public class ClientTest {
 
     Client client = createPlayer(test, input, output);
     client.textPlayer.setPlayerName("Green");
-    String msg = client.checkResult();
+    client.checkResult();
     String expected = 
     "This is your player name for this game: Green\n" +
-    "\nSeems like everyone finishes their turn.\n" +
+    "Congratulations! You successfully own the Territory B\n\n" + 
     "Now let's check the result of this round...\n" +
     "\nPlayer Green wins!\n" +
     "Game End NOW\n";

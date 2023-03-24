0e4f923706df0d6a68596e427b7041f32729cf8d
mz213 <mz213@duke.edu>
2023-03-24 11:06:00 -0400

    fix attack order while loop

---
+++ shared/src/test/java/edu/duke/ece651/team5/shared/UnitValidRuleCheckerTest.java
@@ -0,0 +1,22 @@
+package edu.duke.ece651.team5.shared;
+
+import org.junit.jupiter.api.Test;
+
+public class UnitValidRuleCheckerTest {
+  @Test
+  void testCheckAttackOrderUnitValid() {
+
+    
+
+  }
+
+  @Test
+  void testCheckMoveOrderUnitValid() {
+
+  }
+
+  @Test
+  void testCheckUnitValid() {
+
+  }
+}

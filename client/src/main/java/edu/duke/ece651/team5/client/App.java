05621d8202f860fe8eba3166c7063c29919353e9
rx50 <rx50@duke.com>
2023-04-04 20:02:38 -0400

    Comment client wrong code

--- client/src/main/java/edu/duke/ece651/team5/client/App.java
+++ client/src/main/java/edu/duke/ece651/team5/client/App.java
@@ -11,8 +11,5 @@ import edu.duke.ece651.team5.shared.MyName;
 
 public class App {
   public static void main(String[] args) {
-    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    // Client client = new Client(input, System.out);
-    // client.play();
   }
 }

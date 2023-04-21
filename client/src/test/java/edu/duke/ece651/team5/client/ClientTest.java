e5619642576a66ea5a3df4c457e27eff8ab6ffce
Santiago Brito <sjb103@duke.edu>
2023-04-20 20:44:58 -0400

    added tests for ClientTest

--- client/src/test/java/edu/duke/ece651/team5/client/ClientTest.java
+++ client/src/test/java/edu/duke/ece651/team5/client/ClientTest.java
@@ -14,13 +14,16 @@ import java.io.PrintStream;
 import java.io.StringReader;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import edu.duke.ece651.team5.shared.Action;
 import edu.duke.ece651.team5.shared.PlayerConnection;
+import edu.duke.ece651.team5.shared.game.Game;
 import edu.duke.ece651.team5.shared.game.RISKMap;
 
 public class ClientTest {
@@ -134,6 +137,178 @@ public class ClientTest {
     ArrayList<Integer> messageReceived = client.getJoinableGames();
 
     assertEquals(gameIDs, messageReceived);
+  }
+
+  @Test
+  public void joinNewGameTest() throws IOException, ClassNotFoundException {
+
+    String message = "Joined Success";
+    Integer gameID = 2;
+    when(test.readData()).thenReturn(message);
+    String messageReceived = client.joinNewGame(gameID);
+
+    assertEquals(message, messageReceived);
+  }
+
+  @Test
+  public void getGameTest() throws IOException, ClassNotFoundException {
+
+    Game mockedGame = mock(Game.class);
+
+    when(test.readData()).thenReturn(mockedGame);
+    Game messageReceived = client.getGame();
+
+    assertEquals(mockedGame, messageReceived);
+  }
+
+  @Test
+  public void updateGameAfterTurnTest() throws IOException, ClassNotFoundException {
+
+    Game mockedGame = mock(Game.class);
+
+    when(test.readData()).thenReturn(mockedGame);
+    Game messageReceived = client.updatedGameAfterTurn();
+
+    assertEquals(mockedGame, messageReceived);
+  }
+
+  @Test
+  public void getGameConfirmationTest() throws IOException, ClassNotFoundException {
+
+    String message = "Joined Success";
+
+    when(test.readData()).thenReturn(message);
+    String messageReceived = client.getGameConfirmation();
+
+    assertEquals(message, messageReceived);
+  }
+
+  @Test
+  public void getGameIDTest() throws IOException, ClassNotFoundException {
+
+    // String message = "Joined Success";
+    Integer gameID = 2;
+    when(test.readData()).thenReturn(gameID);
+    Integer messageReceived = client.getGameID();
+
+    assertEquals(gameID, messageReceived);
+  }
+
+  @Test
+  public void signUpTest() throws IOException, ClassNotFoundException {
+
+    String expectedMessage = "Signup confirmed";
+    when(test.readData()).thenReturn(expectedMessage + "\n");
+    String userName = "user";
+    String password = "password";
+    ArrayList<String> userAndPassoword = new ArrayList<>();
+    userAndPassoword.add(userName);
+    userAndPassoword.add(password);
+
+    String actualMessage = client.signUp(userAndPassoword);
+
+    assertEquals(expectedMessage, actualMessage);
+    verify(test, times(1)).readData();
+  }
+
+  @Test
+  public void LogOutfromXTest() throws IOException {
+    client.logOutfromX();
+
+    verify(test).writeData("Log out");
+    verify(test).close();
+  }
+
+  @Test
+  public void sendPlacementOrderTest() throws IOException, ClassNotFoundException {
+
+    String message = "Joined Success";
+    Integer gameID = 4;
+    HashMap<String, Integer> placementOrders = new HashMap<>();
+
+    when(test.readData()).thenReturn(message);
+    String messageReceived = client.sendPlacementOrder(gameID, placementOrders);
+
+    assertEquals(message, messageReceived);
+  }
+
+  @Test
+  public void recvUpdatedGameTest() throws IOException, ClassNotFoundException {
+
+    Game mockedGame = mock(Game.class);
+
+    when(test.readData()).thenReturn(mockedGame);
+    Game messageReceived = client.recvUpdatedGame();
+
+    assertEquals(mockedGame, messageReceived);
+  }
+
+  @Test
+  public void checkWinTest() throws IOException, ClassNotFoundException {
+
+    String message = "You won";
+
+    when(test.readData()).thenReturn(message);
+    String messageReceived = client.checkWin();
+
+    assertEquals(message, messageReceived);
+  }
+
+  @Test
+  public void checkLostTest() throws IOException, ClassNotFoundException {
+
+    String message = "You lost";
+
+    when(test.readData()).thenReturn(message);
+    String messageReceived = client.checkLost();
+
+    assertEquals(message, messageReceived);
+  }
+
+  @Test
+  public void continueGameTest() throws IOException, ClassNotFoundException {
+
+    String message = "You can continue";
+    Integer gameToContinue = 4;
+
+    when(test.readData()).thenReturn(message);
+    String messageReceived = client.continueGame(gameToContinue);
+
+    assertEquals(message, messageReceived);
+  }
+
+  @Test
+  public void sendOrder() throws IOException, ClassNotFoundException {
+
+    String message = "You can continue";
+    Integer gameID = 4;
+    Action action = new Action(null, null, null, null);
+    when(test.readData()).thenReturn(message);
+    String messageReceived = client.sendOrder(gameID, action);
+
+    assertEquals(message, messageReceived);
+  }
+
+  @Test
+  public void sendGameIDTest() throws IOException, ClassNotFoundException {
+
+    String message = "game ID exists";
+    Integer gameID = 2;
+    when(test.readData()).thenReturn(message);
+    String messageReceived = client.sendGameID(gameID);
+
+    assertEquals(message, messageReceived);
+  }
+
+  @Test
+  public void getGamesStartedTest() throws IOException, ClassNotFoundException {
+
+    // String message = "game ID exists";
+    ArrayList<Integer> retrievedGames = new ArrayList<>();
+    when(test.readData()).thenReturn(retrievedGames);
+    ArrayList<Integer> messageReceived = client.getGamesStarted();
+
+    assertEquals(retrievedGames, messageReceived);
   }
 
 }
